370e34fff4881ff80e987c3effc9910aaaf4e1a5
jksr <jksr.tw@gmail.com>
2016-05-31 17:47:08 -0500

    ~

--- pipe_struct.py
+++ pipe_struct.py
@@ -15,16 +15,16 @@ if __name__ == '__main__':
 	
 	for pdb in pdbs:
 		print pdb
-		### make folder for the pdb
-		#os.system('mkdir inputs/'+pdb)
-		### download and process opm pdb
-		#os.system('python scripts/get_opm_pdb.py '+pdb)
-		### get dssp
-		#os.system('mkdssp inputs/'+pdb+'/'+pdb+'.pdb > inputs/'+pdb+'/'+pdb+'.dssp')
-		### get res
-		#os.system('python scripts/get_res.py '+pdb+' '+pdb_info[pdb]['chain'])
-		### get strand intervals
-		#os.system('python scripts/get_strands.py '+pdb+' '+pdb_info[pdb]['chain'])
+		## make folder for the pdb
+		os.system('mkdir inputs/'+pdb)
+		## download and process opm pdb
+		os.system('python scripts/get_opm_pdb.py '+pdb)
+		## get dssp
+		os.system('mkdssp inputs/'+pdb+'/'+pdb+'.pdb > inputs/'+pdb+'/'+pdb+'.dssp')
+		## get res
+		os.system('python scripts/get_res.py '+pdb+' '+pdb_info[pdb]['chain'])
+		## get strand intervals
+		os.system('python scripts/get_strands.py '+pdb+' '+pdb_info[pdb]['chain'])
 
 		## get the center of each strand
 		os.system('python scripts/get_cen.py '+pdb+' '+pdb_info[pdb]['chain'])
